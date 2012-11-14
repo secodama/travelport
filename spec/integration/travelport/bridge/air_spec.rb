f8a1909bf89229086da02530a849ee587128f69a
Paul Gallagher <gallagher.paul@gmail.com>
2012-11-14 11:24:43 +0800

    redo details query with an available response

--- spec/integration/travelport/bridge/air_spec.rb
+++ spec/integration/travelport/bridge/air_spec.rb
@@ -6,10 +6,8 @@ describe Travelport::Bridge::Air do
 
   context 'general search query' do
     use_vcr_cassette
-    it 'should send base search request' do
-      resp = bridge.low_fare_search([{from:'IEV', to:'NYC', at:Time.new + 5.days }, {from:'NYC', to:'IEV', at:Time.new+10.days}], {adults:1, cabin:'Economy'})
-      resp.should be_a(Travelport::Response::LowFareSearchRsp)
-    end
-
+    subject { bridge.low_fare_search([{from:'IEV', to:'NYC', at:Time.new + 5.days }, {from:'NYC', to:'IEV', at:Time.new+10.days}], {adults:1, cabin:'Economy'}) }
+    it { should be_a(Travelport::Response::LowFareSearchRsp) }
   end
+
 end
--- spec/integration/travelport/bridge/hotel_spec.rb
+++ spec/integration/travelport/bridge/hotel_spec.rb
@@ -7,18 +7,14 @@ describe Travelport::Bridge::Hotel do
 
   context 'general search query' do
     use_vcr_cassette
-    it 'should send base search request' do
-      resp = bridge.search_availability('NYC', { checkin:Time.new + 5.days, checkout:Time.new+10.days, adults:1, rooms:1})
-      resp.should be_a(Travelport::Response::HotelSearchAvailabilityRsp)
-    end
+    subject { bridge.search_availability('NYC', { checkin:Time.new + 5.days, checkout:Time.new+10.days, adults:1, rooms:1}) }
+    it { should be_a(Travelport::Response::HotelSearchAvailabilityRsp) }
   end
 
   context 'details query' do
     use_vcr_cassette
-    it 'should send base search request' do
-      resp = bridge.hotel_details('HY', '86178', { checkin:Time.new + 5.days, checkout:Time.new+10.days})
-      resp.should be_a(Travelport::Response::HotelDetailsRsp)
-    end
+    subject { bridge.hotel_details('CP', '02743', { checkin:Time.new + 5.days, checkout:Time.new+10.days}) }
+    it { should be_a(Travelport::Response::HotelDetailsRsp) }
   end
 
 end
